c7058ae081d782a428c058cdac9e2b8f185fe864
Conchylicultor <etiennefg.pot@gmail.com>
2014-10-08 12:33:48 +0900

    Clean code (remove omp)

--- src/main.cpp
+++ src/main.cpp
@@ -35,17 +35,6 @@ int main()
      *
      * */
 
-    /*int nthreads=0;             // number of thread available
-    int threadid=0;             // id of thread
-
-    #pragma omp parallel private(threadid)
-    {
-        nthreads = omp_get_num_threads(); // compute the total number of threads
-        //cout << nthreads << " thread(s) available for computation"<<endl;
-        threadid = omp_get_thread_num();
-        //cout << "Thread " << threadid << " ready" <<endl;
-    }*/
-
     bool recording = false;
 
     Camera *cam1 = 0;
@@ -56,41 +45,13 @@ int main()
     cam2 = new Camera("/home/etienne/__A__/Data/Recordings/02_Vid_2.mp4",recording);
     cam3 = new Camera("/home/etienne/__A__/Data/Recordings/02_Vid_3.mp4",recording);
 
-    /*#pragma omp parallel sections
-    {
-        #pragma omp section
-        {
-            cam1 = new Camera("http://root:azerty@192.168.100.190/axis-cgi/mjpg/video.cgi?type=.mjpg",recording);
-        }
-
-        #pragma omp section
-        {
-            cam2 = new Camera("http://root:azerty@192.168.100.191/axis-cgi/mjpg/video.cgi?type=.mjpg",recording);
-        }
-
-        #pragma omp section
-        {
-            cam3 = new Camera("http://root:azerty@192.168.100.192/axis-cgi/mjpg/video.cgi?type=.mjpg",recording);
-        }
-    }*/
-
     vector<Camera*> listCam;
     listCam.push_back(cam1);
     listCam.push_back(cam2);
     listCam.push_back(cam3);
 
-    // Clear the buffer of each cam
-    /*for(int i = 0 ; i < 150 ; ++i)
-    {
-        for(int j = 0 ; j < listCam.size() ; ++j)
-        {
-            listCam.at(j)->grab();
-        }
-    }*/
-
     while(1)
     {
-        //#pragma omp parallel for
         for(size_t i = 0 ; i < listCam.size() ; ++i)
         {
             listCam.at(i)->grab();
