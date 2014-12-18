c1efe4d8625b9536fcfb1066d9203b8b9593b44a
Conchylicultor <etiennefg.pot@gmail.com>
2014-12-18 18:10:12 +0900

    Load videos/camera and configuration from a yml file

--- src/main.cpp
+++ src/main.cpp
@@ -12,46 +12,29 @@ int main()
 {
     cout << "Loading camera..." << endl;
 
-    /* ---- Instructions ----
-     *
-     * __Cameras__:
-     * Login: root
-     * Password: azerty
-     *
-     * __Urls__:
-     * MotionJpg:  http://root:azerty@192.168.100.190/axis-cgi/mjpg/video.cgi?resolution=640x480;type=.mjpg
-     * Mpeg4:      rtsp://root:azerty@192.168.100.191/mpeg4/media.amp
-     * H.264:      rtsp://root:azerty@192.168.100.192/axis-media/media.amp?tcp
-     * StillImage:
-     *
-     * __Wifi__:
-     * PPID:wfpot WPA2:123456789
-     * PPID:12345 WPA2:123456789
-     *
-     * __Recording__:
-     * Set recording at true
-     * Choose a destination directory in camera.cpp
-     *
-     *
-     * */
-
     bool recording = false;
-
-    Camera *cam1 = 0;
-    Camera *cam2 = 0;
-    Camera *cam3 = 0;
-
-    cam1 = new Camera("/home/etienne/__A__/Data/Recordings/01_Vid_1.mp4",recording);
-    cam2 = new Camera("/home/etienne/__A__/Data/Recordings/01_Vid_2.mp4",recording);
-    cam3 = new Camera("/home/etienne/__A__/Data/Recordings/01_Vid_3.mp4",recording);
-    //cam1 = new Camera("http://root:azerty@192.168.100.168/axis-cgi/mjpg/video.cgi?;type=.mjpg",recording);
-    //cam2 = new Camera("http://root:azerty@192.168.100.169/axis-cgi/mjpg/video.cgi?;type=.mjpg",recording);
-    //cam3 = new Camera("http://root:azerty@192.168.100.170/axis-cgi/mjpg/video.cgi?;type=.mjpg",recording);
-
     vector<Camera*> listCam;
-    listCam.push_back(cam1);
-    listCam.push_back(cam2);
-    listCam.push_back(cam3);
+
+    // Set up configuration
+    FileStorage fileConfig("../video.yml", FileStorage::READ);
+    if(!fileConfig.isOpened())
+    {
+        cout << "Error: No congiguration file" << endl;
+    }
+
+    if(!fileConfig["recording"].empty())
+    {
+        fileConfig["recording"] >> recording;
+    }
+
+    FileNode nodeVideoNames = fileConfig["videoNames"];
+
+    cout << nodeVideoNames.size() << endl;
+    for(string currentName : nodeVideoNames)
+    {
+        cout << currentName << endl;
+        listCam.push_back(new Camera(currentName, recording));
+    }
 
     // Clear the buffer of each cam
     /*for(int i = 0 ; i < 150 ; ++i)
